adfd6c74a23799cabd500ff15ec080d13c5c345c
Mohammedalgzeri <m1h2a3m4m5e6d@gmail.com>
2022-01-30 18:35:06 +0200

    my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoItems from './components/TodoItems/TodoItems'
+import AddItem from './components/AddItem/AddItem'
+import './index.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  
+  let [state, setState] = useState({items:[],inputValue:""});
+//use useEffect to call fetch
+    //use fetch function to call api
+    //console the result of the api
+  useEffect(() => {
+    let response = fetch(
+      "https://61eeb257d593d20017dbb06c.mockapi.io/abi/students",
+      {
+          method: "GET",
+      }
+    );
+    response
+    .then((res) => {
+        return res.json();
+})
+    .then((students) => {
+        console.log(students);
+        setState((prevState) => {
+          return{
+            ...prevState,
+            items: students
+          }
+        })
+
+    })
+    .catch((err) => {
+        console.log(err, "err");
+    });
+  }, []);
+  
+  const addItem = (newStudent) => {
+    const result = fetch(
+        "https://61eeb257d593d20017dbb06c.mockapi.io/abi/students",
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "concent-type": "application/json",
+            },
+            body: JSON.stringify({
+                name: 'newStudent.name',
+                age: 'newStudent.age',
+                id: 5776554445
+            }),
+        }
+    ).then((res) =>{
+      return res.json();
+  })
+  .then((students) => {
+      console.log(students, 'jjlkjlk');
+      setState((prevState) => {
+          return {
+            
+              items: [...prevState.items,students]
+          };
+      });
+  })
+  .catch((err) =>{
+      console.log(err);
+  });
+};
+
+  const deleteItem = (id) => {
+    let items = state.items.filter(item => {
+      return item.id !== id
+    })
+    setState((prevState) => {
+     return{
+      ...prevState,
+      items
+     }           
+    })
+  }
+
+  
+  
+
+  return(
+    <div className="App container">
+       <h1 className="text-center">Class</h1>
+       <TodoItems items={state.items} deleteItem={deleteItem} />
+       <AddItem addItem={addItem} />
     </div>
   );
 }
-
 export default App;
